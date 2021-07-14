4fb4c45563dd9038af3b161dfc7ead7d9e512c97
Lucas Diniz <lucasalvarengadiniz@gmail.com>
2021-07-14 18:09:43 -0300

    show-user link github

--- src/components/ShowUser/ShowUser.js
+++ src/components/ShowUser/ShowUser.js
@@ -17,6 +17,7 @@ class ShowUser extends React.Component {
           <p className="location">{this.props.user.location}</p>
           <p className="bio">{this.props.user.bio}</p>
           <a href={this.props.user.blog} className="blog">{this.props.user.blog}</a>
+          <a href={this.props.user.html_url} className="html_url">{this.props.user.html_url}</a>
           <div className="spans">
             <p>Seguidores: <span>{this.props.user.followers}</span></p>
             <p>Seguindo: <span>{this.props.user.following}</span></p>
