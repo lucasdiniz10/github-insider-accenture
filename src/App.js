efa2cc463849bad42b711f1b9ba77b606d0475a0
Lucas Diniz <lucasalvarengadiniz@gmail.com>
2021-07-14 11:13:41 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import React from 'react';
+import Header from './components/Header/Header';
+import './styles/global.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    text: '',
+    user: {},
+  }
+
+  handleTextChange(e) {
+    this.setState({
+      text: e.target.value
+    })
+  }
+
+  async handleUserSearch(e) {
+    e.preventDefault()
+
+    const emptyField = this.state.text.trim() === '';
+
+    if (!emptyField) {
+      const githubUser = await axios.get(`https://api.github.com/users/${this.state.text}`)
+        .then(response => response.data)
+
+      this.setState({
+        user: githubUser,
+      })
+    } else {
+      alert("Preencha o campo de busca corretamente.");
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <main>
+          <h2>Busque usuários no <span>Github</span></h2>
+          <form onSubmit={this.handleUserSearch.bind(this)}>
+            <input type="text" onChange={this.handleTextChange.bind(this)} />
+            <button type="submit">Buscar Usuário</button>
+          </form>
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
